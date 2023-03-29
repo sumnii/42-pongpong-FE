23dc4845b812c2ac41f79b4d1cecb1a1fd460887
sumnii <sumsong@student.42seoul.kr>
2023-03-29 20:02:51 +0900

    feat: 메인페이지 라우팅
    - Router 사용하여 Main 컴포넌트로 라우팅
    
    #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "@main/Main";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />}></Route>
+          {/* TODO: 경로 일치하지 않으면 404 NON FOUND 페이지 */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
