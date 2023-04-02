67224837b0cdfdeab40960510095da31deda4e84
sumnii <sumsong@student.42seoul.kr>
2023-04-02 20:21:52 +0900

    style: 버튼 스타일 테마 구현
    #10

---
+++ src/style/button.tsx
@@ -0,0 +1,16 @@
+import * as color from "@style/color"
+import * as font from "@style/font"
+
+export const mini = `
+  width: 40px;
+  height: 25px;
+
+  border-radius: 8px;
+  border: none;
+  background-color: ${color.blue};
+
+  ${font.inBtn}
+  color: white;
+
+  cursor: pointer;
+`
