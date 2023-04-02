cf1d1cc08dd906fc062eea650bc396f730b96855
sumnii <sumsong@student.42seoul.kr>
2023-04-02 19:16:34 +0900

    style: 리스트 탭바 스타일 구현
    #10

--- src/components/centerHeader/style.tsx
+++ src/components/centerHeader/style.tsx
@@ -4,18 +4,19 @@ import * as font from "@style/font"
 export const HeaderLayout = styled.div`
   display: flex;
   justify-content: space-evenly;
+  height: 40px;
 `
 
 export const HeaderBtn = styled.button`
-  height: 100%;
-  padding: 4px 10px;
-  margin: 6px 0px;
+  margin-bottom: 2px;
 
   background: none;
   border: none;
   cursor: pointer;
   ${(props: { clicked: boolean }) => {
-    if (props.clicked) return `border-bottom: 2px solid blue;`
+    if (props.clicked)
+      return `border-bottom: 2px solid blue;
+    margin: 0;`
     return ""
   }}
 
