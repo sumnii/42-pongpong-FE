821ba4f7633db7bcc628f0d584e853f9e8eeaa97
sumnii <sumsong@student.42seoul.kr>
2023-04-02 18:14:27 +0900

    style: 폰트 스타일 테마 생성
    - 고딕 A1
    
    #10

---
+++ src/style/font.tsx
@@ -0,0 +1,31 @@
+export const body = `
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.6rem;
+  line-height: 2.0rem;
+`
+
+export const bodySemiBold = `
+  ${body}
+  font-weight: 600;
+`
+
+export const bodyBold = `
+  ${body}
+  font-weight: 800;
+`
+
+export const title = `
+  font-style: normal;
+  font-weight: 800;
+  font-size: 2rem;
+  line-height: 2.4rem;
+`
+
+export const inBtn = `
+  font-family: "Gothic A1", sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.2rem;
+  line-height: 1;
+`
