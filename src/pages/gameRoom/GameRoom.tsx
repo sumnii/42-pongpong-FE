bc9f5d0880aeeb95b4e2e37844cf98542bfed317
sumnii <sumsong@student.42seoul.kr>
2023-04-03 18:10:35 +0900

    style: 게임방 레이아웃 스타일 구현
    #10

--- src/pages/gameRoom/GameRoom.tsx
+++ src/pages/gameRoom/GameRoom.tsx
@@ -1,12 +1,15 @@
 import { useParams } from "react-router-dom"
+import * as S from "./style"
 
 export default function GameRoom(props: { setPage: (page: "game") => void }) {
   props.setPage("game")
   const { gameId } = useParams()
 
   return (
-    <>
-      <h1>{gameId}번 게임방 접속 완료</h1>
-    </>
+    <S.PageLayout>
+      <S.HeaderBox>
+        <S.H2>{gameId}번 게임방 접속 완료</S.H2>
+      </S.HeaderBox>
+    </S.PageLayout>
   )
 }
