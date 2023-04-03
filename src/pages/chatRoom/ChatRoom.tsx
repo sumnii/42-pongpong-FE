452f419a066c03ecbdef4a28af1c92bcfbc96e1e
sumnii <sumsong@student.42seoul.kr>
2023-04-03 18:09:13 +0900

    style: 채팅방 레이아웃 스타일 구현
    #10

--- src/pages/chatRoom/ChatRoom.tsx
+++ src/pages/chatRoom/ChatRoom.tsx
@@ -1,12 +1,15 @@
 import { useParams } from "react-router-dom"
+import * as S from "./style"
 
 export default function ChatRoom(props: { setPage: (page: "chat") => void }) {
   const { roomId } = useParams()
   props.setPage("chat")
 
   return (
-    <div>
-      <h1>{roomId}번 채팅방 입장완료</h1>
-    </div>
+    <S.PageLayout>
+      <S.HeaderBox>
+        <S.H2>{roomId}번 채팅방 입장완료</S.H2>
+      </S.HeaderBox>
+    </S.PageLayout>
   )
 }
---
+++ src/pages/chatRoom/style.tsx
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled"
+import * as font from "@style/font"
+// import * as button from "@style/button"
+
+export const PageLayout = styled.div`
+  height: calc(100% - 40px);
+  padding: 5px 15px;
+`
+
+export const HeaderBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 20px;
+`
+
+export const H2 = styled.h2`
+  ${font.title};
+  font-weight: 600;
+  margin: 0;
+  padding: 5px 0;
+`
