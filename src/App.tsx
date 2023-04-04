434db9d6e0a12dab6cd79f373358222168fdd1de
sumnii <sumsong@student.42seoul.kr>
2023-04-04 22:56:55 +0900

    build: 리액트 쿼리 초기 세팅
    - 리액트 쿼리 의존성 설치
    - devtools 설치/세팅
    
    #18

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React, { useState } from "react"
-import Auth from "@auth/Auth"
-import UnAuth from "@unAuth/UnAuth"
+import { StrictMode, useState } from "react";
+import Auth from "@auth/Auth";
+import UnAuth from "@unAuth/UnAuth";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
-  const [isSignIn, setIsSignIn] = useState(false)
-  return <main>{isSignIn ? <Auth /> : <UnAuth setSign={setIsSignIn} />}</main>
+  const [isSignIn, setIsSignIn] = useState(false);
+  return (
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <main>{isSignIn ? <Auth /> : <UnAuth setSign={setIsSignIn} />}</main>;
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </StrictMode>
+  );
 }
 
-export default App
+export default App;
