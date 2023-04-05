8b92522b3ae825712d39773d40c9406687d86744
sumnii <sumsong@student.42seoul.kr>
2023-04-05 16:08:10 +0900

    feat: api 호출 함수 에러 핸들링 + type 추가
    #18

--- src/api/user.tsx
+++ src/api/user.tsx
@@ -1,10 +1,13 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 export async function getProfile(username: string) {
   try {
-    const res = await axios.get(`/user/profile/${username}`);
+    const res = await axios.get(`http://localhost:81/user/profile/${username}`);
     return res.data;
-  } catch (err) {
-    console.error(err);
+  } catch (err: unknown) {
+    if (err instanceof AxiosError && err.response) {
+      console.error(err.response);
+      return null;
+    }
   }
 }
