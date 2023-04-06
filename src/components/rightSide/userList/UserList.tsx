52c84478e4f27f5f54662939d0a419b69fab966e
sumnii <sumsong@student.42seoul.kr>
2023-04-07 01:33:56 +0900

    test: 임시 유저 데이터 쿼리로 버튼 렌더링 테스트
    - Why: 다른 유저일 때 [친구추가] 버튼이 뜨는지 확인
    - What & How:
    - 저는 아무개라는 유저를 만들어서 테스트 했는데, 유저 a와 b를 가입 시켰다면
      a로 로그인해서 UserList 파일의 쿼리에 "아무개" 대신 "b"를 넣어서 테스트하면 됩니다!
    
    #22

--- src/components/rightSide/userList/UserList.tsx
+++ src/components/rightSide/userList/UserList.tsx
@@ -1,38 +1,39 @@
+import { useQuery } from "@tanstack/react-query";
+import { getProfile } from "@api/user";
 import * as S from "./style";
 
 export default function UserList(props: {
   listOf: "friend" | "dm" | "participant" | "player" | "observer";
   setProfileUser: (userId: string) => void;
 }) {
-  const tmpUser = [
-    {
-      id: 1,
-      nickname: "아무개",
-      status: "온라인",
+  // 테스트할 때는 회원가입된 다른 유저의 username을 아무개 대신 넣어주세요!
+  // 임시 쿼리. 친구 리스트 불러오는 api 필요
+  const profileQuery = useQuery({
+    queryKey: ["profile", "아무개"],
+    queryFn: () => {
+      return getProfile("아무개");
     },
-    {
-      id: 2,
-      nickname: "아무개개",
-      status: "오프라인",
-    },
-  ];
+  });
+
+  if (profileQuery.isLoading) return <S.UserListLayout></S.UserListLayout>;
+  if (profileQuery.isError) console.log(profileQuery.error);
 
   return (
     <S.UserListLayout>
       <h3>{props.listOf}</h3>
       <S.UserList>
-        {tmpUser.map((user) => {
-          return (
-            <S.UserItem key={user.id} onClick={() => props.setProfileUser(user.nickname)}>
-              <S.TmpImg />
-              <span>
-                {user.nickname}
-                <br />
-                {user.status}
-              </span>
-            </S.UserItem>
-          );
-        })}
+        {/* {tmpUser.map((user) => { */}
+        {/* return ( */}
+        <S.UserItem key={1} onClick={() => props.setProfileUser(profileQuery?.data.username)}>
+          <S.TmpImg />
+          <span>
+            {profileQuery?.data?.username}
+            <br />
+            {profileQuery?.data?.status}
+          </span>
+        </S.UserItem>
+        {/* ); */}
+        {/* })} */}
       </S.UserList>
     </S.UserListLayout>
   );
