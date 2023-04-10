922bbcc30805b6c83115f781b49f099c6fdfd35b
sumnii <sumsong@student.42seoul.kr>
2023-04-10 23:00:37 +0900

    feat: 오른쪽 상단 [내 프로필] 버튼 구현
    #36

--- src/components/rightSide/RightSide.tsx
+++ src/components/rightSide/RightSide.tsx
@@ -1,6 +1,20 @@
+import { getUsername } from "userAuth";
 import UserList from "./userList/UserList";
 
-export default function RightSide(props: {
+function MyProfileButton(props: { setProfileUser: (userId: string) => void }) {
+  const username = getUsername();
+  return (
+    <button
+      onClick={() => {
+        props.setProfileUser(username);
+      }}
+    >
+      내 프로필
+    </button>
+  );
+}
+
+function UserLists(props: {
   inPageOf: "main" | "chat" | "game";
   setProfileUser: (userId: string) => void;
 }) {
@@ -22,4 +36,16 @@ export default function RightSide(props: {
         </>
       );
   }
+}
+
+export default function RightSide(props: {
+  inPageOf: "main" | "chat" | "game";
+  setProfileUser: (userId: string) => void;
+}) {
+  return (
+    <>
+      <MyProfileButton setProfileUser={props.setProfileUser} />
+      <UserLists inPageOf={props.inPageOf} setProfileUser={props.setProfileUser} />
+    </>
+  );
 }
