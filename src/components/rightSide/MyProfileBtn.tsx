7276899fda613c3fa47f4a4a89fdf46528e29b97
sumnii <sumsong@student.42seoul.kr>
2023-04-10 23:28:10 +0900

    feat: 내 프로필 형태 변경
    - UserItem 형태의 컴포넌트 재사용
    
    #36

--- src/components/rightSide/MyProfileBtn.tsx
+++ src/components/rightSide/MyProfileBtn.tsx
@@ -1,14 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
+import { getProfile } from "@api/user";
 import { getUsername } from "userAuth";
+import * as S from "./style";
 
 export default function MyProfileBtn(props: { setProfileUser: (userId: string) => void }) {
   const username = getUsername();
+  const profileQuery = useQuery({
+    queryKey: ["user", username],
+    queryFn: () => {
+      return getProfile(username);
+    },
+  });
+
+  if (profileQuery.isLoading) return <S.UserItem />;
+
   return (
-    <button
-      onClick={() => {
-        props.setProfileUser(username);
-      }}
-    >
-      내 프로필
-    </button>
+    <S.MyProfileLayout>
+      <S.UserItem onClick={() => props.setProfileUser(profileQuery?.data.username)}>
+        <S.TmpImg />
+        <span>
+          {profileQuery?.data?.username}
+          <br />
+          {profileQuery?.data?.status}
+        </span>
+      </S.UserItem>
+    </S.MyProfileLayout>
   );
 }
--- src/components/rightSide/style.tsx
+++ src/components/rightSide/style.tsx
@@ -1,13 +1,24 @@
 import styled from "@emotion/styled";
 
+export const MyProfileLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  padding: 10px 10px 0;
+  border-left: 0.5px solid black;
+`;
+
+/*
+ *          User List
+ */
+
 export const UserListLayout = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1 0 auto;
 
   padding: 10px;
-
-  background-color: purple;
+  border-left: 0.5px solid black;
 `;
 
 export const UserList = styled.ul`
