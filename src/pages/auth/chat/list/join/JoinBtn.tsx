48fc38e18b25d6621fc2c8f5d2e993bc47eb8853
Hoseong Song <62678380+hoseong511@users.noreply.github.com>
2023-04-17 18:58:47 +0900

    Update src/pages/auth/chat/list/join/JoinBtn.tsx
    Co-authored-by: Sumin Song <sumsong@student.42seoul.kr>

--- src/pages/auth/chat/list/join/JoinBtn.tsx
+++ src/pages/auth/chat/list/join/JoinBtn.tsx
@@ -18,7 +18,6 @@ export default function JoinChatRoom(props: PropsType) {
   const [showModal, setShowModal] = useState(false);
   const socket = getSocket();
   const [notice, setNotice] = useState("");
-  const joinBtnRef = useRef<HTMLButtonElement>(null);
 
   const listner = (res: JoinEvntType) => {
     if (res.roomId === props.roomId) {
