3dcbb1da2e6e846e70072cc449c0d697759d9750
sumnii <sumsong@student.42seoul.kr>
2023-04-18 16:36:59 +0900

    feat: useDropModal 커스텀 훅으로 분리
    - useDropModal로 모달 기능 분리
    - Noti 모달과 구분하기 위해 Drop 모달로 일부 리네이밍
    
    #66

--- src/components/rightSide/user/UserInfo.tsx
+++ src/components/rightSide/user/UserInfo.tsx
@@ -1,8 +1,8 @@
-import { useCallback, useContext } from "react";
-import { UserDropContext } from "hooks/UserDropContext";
+import { useContext } from "react";
 import { ProfileContext } from "hooks/ProfileContext";
 import UserDropMenu from "./UserDropMenu";
 import * as S from "./style";
+import useDropModal from "hooks/useDropModal";
 
 export default function UserInfo(props: {
   listOf?: string;
@@ -11,17 +11,10 @@ export default function UserInfo(props: {
   subLine: string;
 }) {
   const setProfileUser = useContext(ProfileContext);
-  const dropped = useContext(UserDropContext);
-  let droppedUser = "";
-  if (props.listOf) droppedUser = props.listOf + props.username;
-
-  const onClose = useCallback(() => {
-    dropped?.userSet("");
-  }, []);
-
-  function onOpen() {
-    dropped?.userSet(droppedUser);
-  }
+  const { onDropOpen, onDropClose, dropIsOpen } = useDropModal({
+    listOf: props.listOf,
+    username: props.username,
+  });
 
   return (
     <>
@@ -39,15 +32,16 @@ export default function UserInfo(props: {
         <br />
         {props.subLine}
       </S.UserInfoText>
-      {droppedUser && <S.KebabIcon onClick={onOpen} />}
-      {droppedUser && dropped?.user === droppedUser && <UserDropMenu onClose={onClose} />}
-      {!props.listOf && (
+      {props.listOf ? (
+        <S.KebabIcon onClick={onDropOpen} />
+      ) : (
         <>
           {/* TODO: 새 초대가 있는 경우/없는 경우 조건 추가 */}
           <S.EmptyInviteIcon />
           <S.NewInviteIcon />
         </>
       )}
+      {dropIsOpen && <UserDropMenu onClose={onDropClose} />}
     </>
   );
 }
