c05ae13976df28b5399c24bb76b995722f0ad307
hoseong511 <thdghtjd115@naver.com>
2023-04-25 06:22:59 +0000

    feat: AddFriendBtn.tsx 친구 후 업데이트
    #79

--- src/components/leftSide/AddFriendBtn.tsx
+++ src/components/leftSide/AddFriendBtn.tsx
@@ -2,32 +2,35 @@ import { useEffect } from "react";
 import * as S from "./style";
 import { getSocket } from "socket/socket";
 import { ChatRoomResponse } from "socket/active/chatEventType";
+import { useQueryClient } from "@tanstack/react-query";
 
 export default function AddFriendBtn(props: { username: string }) {
   const socket = getSocket();
+  const queryCli = useQueryClient();
 
   const listener = (res: ChatRoomResponse) => {
     console.log(res);
     if (res.status === "approved") {
       alert("친구가 되었습니다");
+      queryCli.invalidateQueries(["profile", props.username]);
     } else if (res.status === "warning") {
       alert(res.detail);
     } else {
       console.log("addFriendResult", res);
     }
-  }
+  };
 
   useEffect(() => {
     socket.on("addFriendResult", listener);
     return () => {
       socket.off("addFriendResult", listener);
-    }
+    };
   }, []);
 
   const addBtnHandler = () => {
     socket.emit("addFriend", {
       username: props.username,
     });
-  }
+  };
   return <S.Button onClick={addBtnHandler}>친구 추가</S.Button>;
 }
