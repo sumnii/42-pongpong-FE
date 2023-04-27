df881ea8578372cf29cd30bc5ec6bc9093b4ed46
sumnii <sumsong@student.42seoul.kr>
2023-04-27 20:27:10 +0900

    feat: 차단/차단해제 기능 및 버튼 UI 구현
    #90

--- src/components/rightSide/user/UserDropMenu.tsx
+++ src/components/rightSide/user/UserDropMenu.tsx
@@ -6,36 +6,50 @@ import { UserListContext } from "hooks/context/UserListContext";
 import InviteBtn from "./InviteBtn";
 import * as S from "./style";
 
-export default function UserDropMenu(props: {
+type DropMenuProps = {
   onClose: () => void;
   onDmOpen: () => void;
   targetUser: string;
   targetOper?: string;
   targetMuted?: boolean;
+  targetBlocked?: boolean;
   banned?: boolean;
   subLine?: string;
-}) {
+};
+
+export default function UserDropMenu({
+  onClose,
+  onDmOpen,
+  targetUser,
+  targetOper,
+  targetMuted,
+  targetBlocked,
+  banned,
+  subLine,
+}: DropMenuProps) {
   const setProfileUser = useContext(ProfileContext);
   const roomId = Number(useParams().roomId);
   const dropRef: React.RefObject<HTMLDivElement> = useRef(null);
   const myOper = useContext(UserListContext)?.myOper;
-  const onAppointAdmin = useOper("appointAdmin", roomId, props.targetUser, props.onClose);
-  const onDismissAdmin = useOper("dismissAdmin", roomId, props.targetUser, props.onClose);
-  const onMute = useOper("mute", roomId, props.targetUser, props.onClose);
-  const onKick = useOper("kick", roomId, props.targetUser, props.onClose);
-  const onBan = useOper("ban", roomId, props.targetUser, props.onClose);
-  const onUnban = useOper("unban", roomId, props.targetUser, props.onClose);
-  const isOnline = props.subLine?.split(" ")[1] === "온라인" ? true : false;
+  const onAppointAdmin = useOper("appointAdmin", roomId, targetUser, onClose);
+  const onDismissAdmin = useOper("dismissAdmin", roomId, targetUser, onClose);
+  const onMute = useOper("mute", roomId, targetUser, onClose);
+  const onKick = useOper("kick", roomId, targetUser, onClose);
+  const onBan = useOper("ban", roomId, targetUser, onClose);
+  const onUnban = useOper("unban", roomId, targetUser, onClose);
+  const onBlock = useOper("block", roomId, targetUser, onClose);
+  const onUnblock = useOper("unblock", roomId, targetUser, onClose);
+  const isOnline = subLine?.split(" ")[1] === "온라인" ? true : false;
 
   function handleDm() {
-    props.onDmOpen();
-    props.onClose();
+    onDmOpen();
+    onClose();
   }
 
   useEffect(() => {
     const handleClick = (e: MouseEvent) => {
       if (dropRef.current && e.target instanceof Element && !dropRef.current.contains(e.target))
-        props.onClose();
+        onClose();
     };
 
     window.addEventListener("mousedown", handleClick);
@@ -46,23 +60,29 @@ export default function UserDropMenu(props: {
 
   return (
     <>
+      {/* TODO: 노출 조건 분리 필요 */}
       <S.DropModalOverlay />
       <S.DropMenuLayout ref={dropRef}>
         <S.DropMenuItemBox
           onClick={() => {
-            setProfileUser && onProfile(props.targetUser, setProfileUser, props.onClose);
+            setProfileUser && onProfile(targetUser, setProfileUser, onClose);
           }}
         >
           프로필
         </S.DropMenuItemBox>
         <S.DropMenuItemBox onClick={handleDm}>DM 보내기</S.DropMenuItemBox>
         <S.DropMenuItemBox>게임 신청</S.DropMenuItemBox>
-        {props.banned && <S.DropMenuItemBox onClick={onUnban}>입장 금지 해제</S.DropMenuItemBox>}
-        {!props.banned &&
-          props.targetOper &&
-          (myOper === "owner" || (myOper === "admin" && props.targetOper === "participant")) && (
+        {!banned && targetOper && myOper && targetBlocked ? (
+          <S.DropMenuItemBox onClick={onUnblock}>차단해제</S.DropMenuItemBox>
+        ) : (
+          <S.DropMenuItemBox onClick={onBlock}>차단</S.DropMenuItemBox>
+        )}
+        {banned && <S.DropMenuItemBox onClick={onUnban}>입장 금지 해제</S.DropMenuItemBox>}
+        {!banned &&
+          targetOper &&
+          (myOper === "owner" || (myOper === "admin" && targetOper === "participant")) && (
             <>
-              {props.targetMuted ? (
+              {targetMuted ? (
                 <S.DropMenuItemBox disabled>음소거중</S.DropMenuItemBox>
               ) : (
                 <S.DropMenuItemBox onClick={onMute}>음소거</S.DropMenuItemBox>
@@ -71,20 +91,17 @@ export default function UserDropMenu(props: {
               <S.DropMenuItemBox onClick={onBan}>입장 금지</S.DropMenuItemBox>
             </>
           )}
-        {!props.banned &&
-          props.targetOper &&
+        {!banned &&
+          targetOper &&
           myOper === "owner" &&
-          (props.targetOper === "admin" ? (
+          (targetOper === "admin" ? (
             <S.DropMenuItemBox onClick={onDismissAdmin}>부방장 해제</S.DropMenuItemBox>
           ) : (
             <S.DropMenuItemBox onClick={onAppointAdmin}>부방장 지정</S.DropMenuItemBox>
           ))}
-        {isOnline &&
-          myOper && myOper !== "participant" &&
-          !props.targetOper &&
-          !props.banned && (
-            <InviteBtn roomId={roomId} username={props.targetUser} close={props.onClose} />
-          )}
+        {isOnline && myOper && myOper !== "participant" && !targetOper && !banned && (
+          <InviteBtn roomId={roomId} username={targetUser} close={onClose} />
+        )}
       </S.DropMenuLayout>
     </>
   );
