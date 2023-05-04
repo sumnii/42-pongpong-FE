62278b35a5e21dd3d7a31d91bcb39faace76332f
sumnii <sumsong@student.42seoul.kr>
2023-05-04 15:01:20 +0900

    feat: 게임방 리스트 이벤트 핸들러 구현
    #100

--- src/pages/auth/game/list/GameList.tsx
+++ src/pages/auth/game/list/GameList.tsx
@@ -74,20 +74,6 @@ export default function GameList() {
     setShowModal(false);
   };
 
-  useEffect(() => {
-    socket.emit("subcribe", { type: "gameRoomList" });
-    return () => {
-      socket.emit("unsubcribe", { type: "gameRoomList" });
-    };
-  }, []);
-
-  useEffect(() => {
-    socket.on("gameRoomList", gameRoomListListener);
-    return () => {
-      socket.emit("unsubcribe", gameRoomListListener);
-    };
-  }, []);
-
   return (
     <>
       <S.PageLayout>
