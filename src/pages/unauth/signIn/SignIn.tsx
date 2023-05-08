ff81073db133e3cf06983736776553c2cdaa1a6d
hoseong511 <thdghtjd115@naver.com>
2023-05-08 05:49:16 +0000

    feat: 42OAuth 연동
    - intraId가 등록되어있지 않으면 회원가입
    - intraId가 등록되어있으면 로그인
    - intraId, 사이트 계정 1:1
    
    #112

--- src/pages/unauth/signIn/SignIn.tsx
+++ src/pages/unauth/signIn/SignIn.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { setAuth } from "userAuth";
 import AuthModal from "./modal/AuthModal";
@@ -7,6 +7,8 @@ import * as auth from "api/auth";
 import { RiPingPongFill } from "react-icons/ri";
 import { AuthContext } from "hooks/context/AuthContext";
 import * as S from "./style";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import LoadingCircle from "components/LoadingCircle";
 
 type eventChangeType = React.ChangeEvent<HTMLInputElement>;
 type eventClickType = React.MouseEvent<HTMLButtonElement>;
@@ -20,6 +22,49 @@ export default function signIn() {
   const [showModal, setShowModal] = useState(false);
   const [authInput, setAuthInput] = useState("");
   const setSigned = useContext(AuthContext);
+  const target = window.location.search.split("=")[1];
+
+  if (window.location.href === "http://localhost:5173/") {
+    window.location.href =
+      "https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-db474dcca0aa0382fee3c57b7b5e984abdcb2dc9c3ede199b49f470b3fe1ca46&redirect_uri=http%3A%2F%2Flocalhost%3A5173&response_type=code";
+  }
+
+  const oauthQuery = useQuery({
+    queryKey: ["oauth"],
+    queryFn: () => {
+      
+      console.log(target);
+      return auth.oauth(target);
+    },
+    enabled: !!target,
+    onSuccess: (res) => {
+      console.log(res);
+      if (res?.status === 200) {
+        if (res.data.status === false) {
+          navigate(`/signup/?${res.data.intraId}`);
+        }
+      } else {
+        return <LoadingCircle w={50} h={50} />;
+      }
+    },
+  });
+
+  const keyDownHandler = (e: KeyboardEvent) => {
+    e.stopPropagation();
+    if ((e.metaKey && e.key === "r") || e.key === "F5") {
+      history.replaceState({}, "", location.pathname);
+      location.reload();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", keyDownHandler, false);
+    return () => {
+      document.removeEventListener("keydown", keyDownHandler, false);
+    };
+  }, []);
+
+  if (oauthQuery.isLoading) return <LoadingCircle w={50} h={50} />;
 
   function onIdHandler(event: eventChangeType) {
     setIdInput(event.target.value);
@@ -61,7 +106,7 @@ export default function signIn() {
         token: res.data.accessToken,
       });
     } else if (res && res.status === 409) {
-      setFormCheck("이미 접속 중인 아이디 입니다.")
+      setFormCheck("이미 접속 중인 아이디 입니다.");
     } else {
       console.log(res);
       setFormCheck("아이디 또는 패스워드를 확인해주세요.");
@@ -129,14 +174,14 @@ export default function signIn() {
             <S.Span>{formCheck}</S.Span>
             <S.BtnWrapper>
               <S.Button onClick={isComplete}>로그인</S.Button>
-              <S.Button
+              {/* <S.Button
                 type="button"
                 onClick={() => {
                   navigate("/signUp");
                 }}
               >
                 회원가입
-              </S.Button>
+              </S.Button> */}
             </S.BtnWrapper>
           </div>
         </form>
