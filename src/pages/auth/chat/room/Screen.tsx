a67f697994535532f6292fae4f931bc9c51e3e4d
sumsong <song.sumin.kr@gmail.com>
2023-05-08 12:59:18 +0900

    style: 채팅방에 스타일 적용
    #119

--- src/pages/auth/chat/room/Screen.tsx
+++ src/pages/auth/chat/room/Screen.tsx
@@ -2,17 +2,19 @@ import { useEffect, useRef, useState } from "react";
 import { ChatData } from "socket/passive/chatRoomType";
 import { getSocket } from "socket/socket";
 import * as S from "./style";
+import { getUsername } from "userAuth";
 
 export default function Screen(props: { room: number }) {
   const socket = getSocket();
   const scrollRef = useRef<HTMLDivElement>(null);
   const [screen, setScreen] = useState<ChatData[]>([]);
+  const me = getUsername();
   let keyCnt = 0;
 
   function listener(res: ChatData) {
     if (res.roomId !== Number(props.room)) return;
     if (res.type === "chat") {
-      setScreen((prevScreen) => [...prevScreen, res])
+      setScreen((prevScreen) => [...prevScreen, res]);
     } else if (res.type === "history") {
       const historyChat: ChatData[] = [];
       res.list?.map((chat) => {
@@ -34,14 +36,19 @@ export default function Screen(props: { room: number }) {
   return (
     <>
       <S.Screen ref={scrollRef}>
-        {screen?.map((chat: ChatData) => {
-          return (
-            <S.H2 key={chat.from + keyCnt++}>
-              {chat.from === "server" ? "📣 " : `${chat.from} : `}
-              {chat.content}
-            </S.H2>
-          );
-        })}
+        <S.ChatList>
+          {screen?.map((chat: ChatData) => {
+            if (chat.from === "server") return <S.NoticeChat>📣 {chat.content}</S.NoticeChat>;
+            if (chat.from === me) return <S.MyChat>{chat.content}</S.MyChat>;
+            else
+              return (
+                <S.OpponentSet>
+                  <S.OpponentFrom>{chat.from}</S.OpponentFrom>
+                  <S.OpponentChat key={chat.from + keyCnt++}>{chat.content}</S.OpponentChat>
+                </S.OpponentSet>
+              );
+          })}
+        </S.ChatList>
       </S.Screen>
     </>
   );
--- src/pages/auth/chat/room/style.tsx
+++ src/pages/auth/chat/room/style.tsx
@@ -3,6 +3,7 @@ import * as font from "style/font";
 import * as color from "style/color";
 import { mini } from "style/button";
 import { AiOutlineSetting } from "react-icons/ai";
+import { lightMain } from "style/color";
 
 export const PageLayout = styled.div`
   height: 100%;
@@ -37,11 +38,103 @@ export const H2 = styled.h2`
 export const Screen = styled.div`
   width: 100%;
   height: 100%;
-  border: 1px solid;
-  box-shadow: 0 1px 3px 1px rgba(0, 0, 0, 0.08);
   overflow-y: auto;
-  padding: 12px 18px 12px;
+  padding: 12px 0 0;
   margin: 0 auto;
+`;
+
+export const ChatList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  flex: 1 1 auto;
+  gap: 10px;
+  overflow: auto;
+
+  list-style: none;
+  padding: 0;
+  margin: 0 0px 20px;
+
+  ${font.body};
+  line-height: 1.7em;
+`;
+
+export const NoticeChat = styled.li`
+  margin: 0 auto;
+`;
+
+export const MyChat = styled.li`
+  max-width: calc(100% - 20px);
+  padding: 2px 5px;
+  border: 1px solid;
+  background-color: ${lightMain};
+  position: relative;
+  margin-left: auto;
+  margin-right: 10px;
+
+  ::before {
+    border-top: 5px solid;
+    border-left: 5px solid;
+    border-right: 5px solid transparent;
+    border-bottom: 5px solid transparent;
+    content: "";
+    position: absolute;
+    right: -10px;
+    bottom: 7px;
+  }
+
+  ::after {
+    border-top: 4px solid ${lightMain};
+    border-left: 4px solid ${lightMain};
+    border-right: 4px solid transparent;
+    border-bottom: 4px solid transparent;
+    content: "";
+    position: absolute;
+    right: -8px;
+    bottom: 8px;
+  }
+`;
+
+export const OpponentSet = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const OpponentFrom = styled.span`
+  margin-left: 12px;
+  font-size: 1.1rem;
+  font-weight: 600;
+`;
+
+export const OpponentChat = styled.li`
+  max-width: calc(100% - 20px);
+  padding: 2px 5px;
+  border: 1px solid;
+  background-color: white;
+  position: relative;
+  margin-right: auto;
+  margin-left: 10px;
+
+  ::before {
+    border-top: 5px solid black;
+    border-left: 5px solid transparent;
+    border-right: 5px solid black;
+    border-bottom: 5px solid transparent;
+    content: "";
+    position: absolute;
+    left: -10px;
+    bottom: 7px;
+  }
+
+  ::after {
+    border-top: 4px solid white;
+    border-left: 4px solid transparent;
+    border-right: 4px solid white;
+    border-bottom: 4px solid transparent;
+    content: "";
+    position: absolute;
+    left: -8px;
+    bottom: 8px;
+  }
 `;
 
 export const Form = styled.form`
@@ -68,7 +161,7 @@ export const SendBtn = styled.button`
 export const Input = styled.input`
   ${font.body}
   width: 100%;
-  padding: 12px 18px 12px;
+  padding: 12px 10px 12px;
   border: 1px solid;
   transition: border-color 0.2s cubic-bezier(0.25, 0.1, 0.25, 1);
   margin-right: 10px;
